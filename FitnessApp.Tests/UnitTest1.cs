70006d7ff575629356574d4cc730ef52d6dadf48
station72 <veremeyenko.s@gmail.com>
2021-09-12 09:11:29 +0300

    added unit-test proj to sln

---
+++ FitnessApp.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace FitnessApp.Tests
+{
+    // dotnet new xunit -o FitnessApp.Tests
+    // dotnet sln add .\FitnessApp.Tests\FitnessApp.Tests.csproj
+
+    // https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-dotnet-test
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
